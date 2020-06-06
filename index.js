fe740c4aa286d6349aa1655c515a2aea11e6e3ec
VarunRaj7 <rvarunr7@gmail.com>
2020-06-06 15:28:44 -0400

    port modifications

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 8080
 
 app.get('/', function (req, res) {
   res.send('Hello Varun!');
 });
 
-app.listen(8080, function () {
-  console.log('simple rest api listening on port 8080!');
+app.listen(PORT, function () {
+  console.log(`simple rest api listening on port ${PORT}!`);
 });
